2ce94e7a6360eff351a2ac010c47dd98b9579f78
NanAHwan <mojiebaiyi@gmail.com>
2019-08-11 10:24:03 +0800

    Add error message Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,2 +1,8 @@
 """Nan A Hwan
 """
+user_name = input("Please enter your name: ")
+if user_name != "":
+    print(user_name[1:len(user_name): 2])
+else:
+    print("Must not be empty.")
+    user_name = input("Please enter your name: ")
